4dade2f335783eba9e1ddfaf3d83538c6d1d841f
YONGHWA\ekore <yonghwa95@naver.com>
2024-02-14 13:34:02 +0900

    session 직접 만들기 복습

---
+++ src/main/java/hello/login/web/SessionManager.java
@@ -0,0 +1,53 @@
+package hello.login.web;
+
+import hello.login.domain.member.Member;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Component
+public class SessionManager {
+
+    public static final String SESSION_COOKIE_NAME = "mySessionId";
+    private final Map<String, Object> sessionStore = new ConcurrentHashMap<>();
+
+    public void createSession(Object value, HttpServletResponse response) {
+        //세션id를 생성하고, 값을 세션에 저장
+        String sessionId = UUID.randomUUID().toString();
+        sessionStore.put(sessionId, value);
+
+        //쿠키 생성
+        Cookie cookie = new Cookie(SESSION_COOKIE_NAME, sessionId);
+        response.addCookie(cookie);
+    }
+
+    public Object getSession(HttpServletRequest request) {
+        Cookie cookie = findCookie(request, SESSION_COOKIE_NAME);
+        if (cookie == null) {
+            return null;
+        }
+        return sessionStore.get(cookie.getValue());
+    }
+
+    public void expire(HttpServletRequest request) {
+        Cookie cookie = findCookie(request, SESSION_COOKIE_NAME);
+        if (cookie != null) {
+            sessionStore.remove(cookie.getValue());
+        }
+    }
+
+    private Cookie findCookie(HttpServletRequest request, String cookieName) {
+        Cookie[] cookies = request.getCookies();
+        return Arrays.stream(cookies)
+                .filter(cookie -> cookie.getName().equals(cookieName))
+                .findAny()
+                .orElse(null);
+    }
+}
